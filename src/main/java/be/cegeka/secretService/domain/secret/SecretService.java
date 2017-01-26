69ad0a13cc61d9b2b5f097a24e996c83eae5fedd
Roel Goossens <roelgoossens22@hotmail.com>
2017-01-26 10:57:16 +0100

    Added get random secret

--- src/main/java/be/cegeka/secretService/domain/secret/SecretService.java
+++ src/main/java/be/cegeka/secretService/domain/secret/SecretService.java
@@ -2,6 +2,8 @@ package be.cegeka.secretService.domain.secret;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by roelg on 26/01/2017.
@@ -11,5 +13,12 @@ public class SecretService {
 
     @Inject
     SecretRepository secretRepository;
+
+    public Secret getRandomSecret() {
+        List<Secret> secretList = secretRepository.getAllSecrets();
+        Secret output  = secretList.get(new Random().nextInt(secretList.size()));
+        secretRepository.removeSecret(output);
+        return output;
+    }
 
 }
