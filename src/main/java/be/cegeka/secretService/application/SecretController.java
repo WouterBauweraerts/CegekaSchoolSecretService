4c55f54bfbb4c0bb7035b67a12ca2ac0786c85f6
Wouter Bauweraerts <wouter.bauweraerts@cegeka.com>
2017-01-26 16:44:29 +0100

    implemented secretController and new functionality to check wich persons don't have a secret

---
+++ src/main/java/be/cegeka/secretService/application/SecretController.java
@@ -0,0 +1,45 @@
+package be.cegeka.secretService.application;
+
+import be.cegeka.secretService.domain.persons.Person;
+import be.cegeka.secretService.domain.secrets.SecretService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * Created by wouterba on 26/01/2017.
+ */
+@Controller
+@RequestMapping(value = "/secret")
+public class SecretController {
+
+    @Inject
+    private SecretService secretService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void addSecret(
+            @RequestParam(value = "secret") String secret,
+            @RequestParam(value = "firstName") String firstName,
+            @RequestParam(value = "lastName") String lastName){
+        secretService.addSecret(secret, firstName, lastName);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public
+    @ResponseBody
+    ResponseEntity<List<Person>> getPersonsWithoutSecret(){
+        List<Person> personsNoSecret = secretService.getPersonsWithoutSecret();
+
+        if (personsNoSecret.size()==0){
+            return new ResponseEntity<>(personsNoSecret, HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(personsNoSecret, HttpStatus.OK);
+    }
+}
--- src/main/java/be/cegeka/secretService/domain/secrets/SecretService.java
+++ src/main/java/be/cegeka/secretService/domain/secrets/SecretService.java
@@ -5,6 +5,7 @@ import be.cegeka.secretService.domain.persons.PersonService;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -19,18 +20,38 @@ public class SecretService {
     @Inject
     PersonService personService;
 
-    public void addSecret(String secretString, String firstName, String lastName){
-        Person owner = personService.getPerson(firstName,lastName);
-        if (owner == null){
-            owner = personService.addPerson(firstName,lastName);
+    public void addSecret(String secretString, String firstName, String lastName) {
+        Person owner = personService.getPerson(firstName, lastName);
+        if (owner == null) {
+            owner = personService.addPerson(firstName, lastName);
         }
-        secretRepository.addSecret(new Secret(secretString,owner.getEncryptedHash()));
+        secretRepository.addSecret(new Secret(secretString, owner.getEncryptedHash()));
     }
 
     public Secret getRandomSecret() {
-        Secret output  = secretRepository.getRandomSecret();
+        Secret output = secretRepository.getRandomSecret();
         secretRepository.removeSecret(output);
         return output;
     }
 
+    public List<Person> getPersonsWithoutSecret() {
+        List<Person> personsWithoutSecret = new ArrayList<>();
+        for (Person person : personService.getAllPersons()) {
+            if (!personHasSecret(person.getEncryptedHash())) {
+                personsWithoutSecret.add(person);
+            }
+        }
+        return personsWithoutSecret;
+    }
+
+    private boolean personHasSecret(String encryptedHash) {
+        boolean hasSecret = false;
+        for (Secret secret : secretRepository.readAll()) {
+            if (encryptedHash.equals(secret.getOwnerHash())) {
+                hasSecret = true;
+            }
+        }
+
+        return hasSecret;
+    }
 }
