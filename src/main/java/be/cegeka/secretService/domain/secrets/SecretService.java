273537df11341770998e3e1afb4fdcd6ee07be89
Roel Goossens <roelgoossens22@hotmail.com>
2017-01-26 13:54:51 +0100

    Implemented addSecret

--- src/main/java/be/cegeka/secretService/domain/secrets/SecretService.java
+++ src/main/java/be/cegeka/secretService/domain/secrets/SecretService.java
@@ -1,5 +1,8 @@
 package be.cegeka.secretService.domain.secrets;
 
+import be.cegeka.secretService.domain.persons.Person;
+import be.cegeka.secretService.domain.persons.PersonService;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.List;
@@ -13,6 +16,16 @@ public class SecretService {
 
     @Inject
     SecretRepository secretRepository;
+    @Inject
+    PersonService personService;
+
+    public void addSecret(String secretString, String firstName, String lastName){
+        Person owner = personService.getPerson(firstName,lastName);
+        if (owner == null){
+            owner = personService.addPerson(firstName,lastName);
+        }
+        secretRepository.addSecret(new Secret(secretString,owner.getEncryptedHash()));
+    }
 
     public Secret getRandomSecret() {
         List<Secret> secretList = secretRepository.getAllSecrets();
